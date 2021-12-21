fd3552761af7769879d0965ce865c4369f82b8e4
mgmorente <mgmorente@gmail.com>
2021-12-21 12:12:03 +0100

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// componentes
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
+// modulos
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContadorComponent,
   ],
-  imports: [
-    BrowserModule
+  imports: [ // modulos
+    BrowserModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
